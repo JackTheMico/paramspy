25d4093fef4a4df9ca31d628acb6e339ebcf2e18
dengliangwen <jackdeng@dengliangwendeMacBook-Pro.local>
2019-03-22 15:22:37 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="paramspy",
     version="1.0.0",
     author="Jack Deng",
-    keywords= ("parameter", "check"),
+    keywords= ["parameter", "check"],
     author_email="dlwxxxdlw@gmail.com",
     description="Parameter Checker For Pythoner",
     long_description="make parameters check more pythonic",
